4ff0ca363ef3eb65e79e0d0c3de0255c30c1bc15
Jeff Vogelsang <jeffvogelsang@gmail.com>
2013-06-03 18:38:47 -0400

    Initial Commit

---
+++ cookbooks/OpenWRT/recipes/default.rb
@@ -0,0 +1,48 @@
+#
+# Cookbook Name:: OpenWRT
+# Recipe:: default
+#
+# Copyright 2013, Jeff Vogelsang
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt_package "build-essential" do
+  action :install
+end
+
+apt_package "flex" do
+  action :install
+end
+
+apt_package "gawk" do
+  action :install
+end
+
+apt_package "gcc-multilib" do
+  action :install
+end
+
+apt_package "gettext" do
+  action :install
+end
+
+apt_package "git-core" do
+  action :install
+end
+
+apt_package "ia32-libs-multiarch" do
+  action :install
+end
+
+apt_package "libncurses5-dev" do
+  action :install
+end
+
+apt_package "subversion" do
+  action :install
+end
+
+apt_package "zlib1g-dev" do
+  action :install
+end
